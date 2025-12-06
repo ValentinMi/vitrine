f7ca062b30a6b920c9977ad3f7ebaf2e186d58ad
Valentin Misiaszek <mowerv6@gmail.com>
2025-12-06 01:07:10 +0100

    chore: add https conf

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,18 @@
-// @ts-check
+import node from "@astrojs/node";
 import { defineConfig } from "astro/config";
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+	site: "https://webxfactory.fr", // ← IMPORTANT
+	output: "server",
+	adapter: node({
+		mode: "standalone",
+	}),
+	server: {
+		port: 4321,
+		host: "0.0.0.0",
+	},
+	// Force HTTPS pour tous les assets
+	build: {
+		assetsPrefix: "https://webxfactory.fr",
+	},
+});
